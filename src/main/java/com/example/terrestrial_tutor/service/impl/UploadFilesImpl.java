87a7957d5a6a8b3fd1751f347877947e2e97e289
Egor Baryshnikov <ebarishnikov>
2024-03-29 13:15:14 +0500

    Hotfix CORS

--- src/main/java/com/example/terrestrial_tutor/service/impl/UploadFilesImpl.java
+++ src/main/java/com/example/terrestrial_tutor/service/impl/UploadFilesImpl.java
@@ -33,7 +33,6 @@ public class UploadFilesImpl implements UploadFilesService {
 
                 if (!uploadDir.exists()) {
                     uploadDir.mkdirs();
-                    System.out.println(uploadDir.exists());
                 }
 
                 String fileUuid = UUID.randomUUID().toString();
